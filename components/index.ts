121bc106d4cbc96954445fae847131d6545a167f
M4p4 <homejr@protonmail.com>
2022-08-26 13:23:20 +0200

    wip

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { default as Navbar } from './navbar';
 export { default as Layout } from './layout/Layout';
 export { default as NftList } from './nft/list';
 export { default as NftItem } from './nft/item';
+export { default as ActiveLink } from './link';
