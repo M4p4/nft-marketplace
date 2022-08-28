aa4334d528f6e09ac72b15bacd79ce74a83022cc
M4p4 <homejr@protonmail.com>
2022-08-28 12:06:28 +0200

    account changes

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,4 +1,5 @@
 import { HookFactory } from '@_types/hooks';
+import { useEffect } from 'react';
 import useSWR from 'swr';
 
 type useAccountResponse = { connect: () => void };
@@ -26,6 +27,22 @@ export const hookFactory: AccountHookFactory =
         revalidateOnFocus: false,
       }
     );
+
+    useEffect(() => {
+      ethereum?.on('accountsChanged', handleAccountsChanged);
+      return () => {
+        ethereum?.removeListener('accountsChanged', handleAccountsChanged);
+      };
+    });
+
+    const handleAccountsChanged = (...args: unknown[]) => {
+      const accounts = args[0] as string[];
+      if (accounts.length === 0) {
+        console.error('Please connect to Metamask!');
+      } else if (accounts[0] !== swrRes.data) {
+        alert('Account has been changed!');
+      }
+    };
 
     const connect = async () => {
       try {
