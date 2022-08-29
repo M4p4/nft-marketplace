2b8e50a54d9bcf1b507ddf3741b5b937b0c54d55
M4p4 <homejr@protonmail.com>
2022-08-29 18:10:11 +0200

    wip

--- test/NftMarket.test.js
+++ test/NftMarket.test.js
@@ -1,17 +1,19 @@
 const NftMarket = artifacts.require('NftMarket');
 const truffleAssert = require('truffle-assertions');
+const { ethers } = require('ethers');
 
 contract('NftMarket', (accounts) => {
   let _contract = null;
+  let _nftPrice = ethers.utils.parseEther('0.3').toString();
+
   before(async () => {
     _contract = await NftMarket.deployed();
   });
 
   describe('Mint token', () => {
     const tokenURI = 'https://test.com';
-    const price = 5;
     before(async () => {
-      await _contract.mintToken(tokenURI, price, { from: accounts[0] });
+      await _contract.mintToken(tokenURI, _nftPrice, { from: accounts[0] });
     });
 
     it('owner of first token should be address[0]', async () => {
@@ -30,10 +32,15 @@ contract('NftMarket', (accounts) => {
 
     it('tokenURI already used', async () => {
       await truffleAssert.fails(
-        _contract.mintToken(tokenURI, price, { from: accounts[0] }),
+        _contract.mintToken(tokenURI, _nftPrice, { from: accounts[0] }),
         truffleAssert.ErrorType.REVERT,
         'Token URI already exists'
       );
+    });
+
+    it('should have one listed item', async () => {
+      const listedItemsCount = await _contract.listedItemsCount();
+      assert.equal(listedItemsCount.toNumber(), 1, 'listedItemsCount is not 1');
     });
   });
 });
