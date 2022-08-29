c56fbc4becf5a02879f17d9998694cd3069f1242
M4p4 <homejr@protonmail.com>
2022-08-29 17:38:19 +0200

    wip

--- test/NftMarket.test.js
+++ test/NftMarket.test.js
@@ -1,4 +1,5 @@
 const NftMarket = artifacts.require('NftMarket');
+const truffleAssert = require('truffle-assertions');
 
 contract('NftMarket', (accounts) => {
   let _contract = null;
@@ -24,6 +25,14 @@ contract('NftMarket', (accounts) => {
     it('first token should point to the correct tokenURI', async () => {
       const actualTokenURI = await _contract.tokenURI(1);
       assert.equal(actualTokenURI, tokenURI, 'TokenURI does not match');
+    });
+
+    it('tokenURI already used', async () => {
+      await truffleAssert.fails(
+        _contract.mintToken(tokenURI, { from: accounts[0] }),
+        truffleAssert.ErrorType.REVERT,
+        'Token URI already exists'
+      );
     });
   });
 });
