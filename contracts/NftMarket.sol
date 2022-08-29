6a1321823e2f1c4b2234f7aa9e897868b32238f3
M4p4 <homejr@protonmail.com>
2022-08-29 18:20:04 +0200

    finished NftMarket + tests

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -10,15 +10,17 @@ contract NftMarket is ERC721URIStorage {
   Counters.Counter private _listedItems;
   Counters.Counter private _tokenIds;
 
-  mapping(string => bool) private _usedTokenURIs;
-  mapping(uint => NftItem) private _idToNftItem;
-
   struct NftItem {
     uint tokenId;
     uint price;
     address creator;
     bool isListed;
   }
+
+  uint public listingPrice = 0.025 ether;
+
+  mapping(string => bool) private _usedTokenURIs;
+  mapping(uint => NftItem) private _idToNftItem;
 
   event NftItemCreated (
     uint tokenId,
@@ -29,8 +31,21 @@ contract NftMarket is ERC721URIStorage {
 
   constructor() ERC721("CreaturesNFT", "CNFT") {}
 
+  function getNftItem(uint tokenId) public view returns (NftItem memory) {
+    return _idToNftItem[tokenId];
+  }
+
+  function listedItemsCount() public view returns (uint) {
+    return _listedItems.current();
+  }
+
+  function tokenURIExists(string memory tokenURI) public view returns (bool) {
+    return _usedTokenURIs[tokenURI] == true;
+  }
+
   function mintToken(string memory tokenURI, uint price) public payable returns (uint) {
     require(!tokenURIExists(tokenURI), "Token URI already exists");
+    require(msg.value == listingPrice, "Price must be equal to listing price");
 
     _tokenIds.increment();
     _listedItems.increment();
@@ -58,16 +73,4 @@ contract NftMarket is ERC721URIStorage {
     emit NftItemCreated(tokenId, price, msg.sender, true);
   }
 
-  function getNftItem(uint tokenId) public view returns (NftItem memory) {
-    return _idToNftItem[tokenId];
-  }
-
-  function listedItemsCount() public view returns (uint) {
-    return _listedItems.current();
-  }
-
-  function tokenURIExists(string memory tokenURI) public view returns (bool) {
-    return _usedTokenURIs[tokenURI] == true;
-  }
- 
  }
--- test/NftMarket.test.js
+++ test/NftMarket.test.js
@@ -5,6 +5,7 @@ const { ethers } = require('ethers');
 contract('NftMarket', (accounts) => {
   let _contract = null;
   let _nftPrice = ethers.utils.parseEther('0.3').toString();
+  let _listingPrice = ethers.utils.parseEther('0.025').toString();
 
   before(async () => {
     _contract = await NftMarket.deployed();
@@ -13,7 +14,10 @@ contract('NftMarket', (accounts) => {
   describe('Mint token', () => {
     const tokenURI = 'https://test.com';
     before(async () => {
-      await _contract.mintToken(tokenURI, _nftPrice, { from: accounts[0] });
+      await _contract.mintToken(tokenURI, _nftPrice, {
+        from: accounts[0],
+        value: _listingPrice,
+      });
     });
 
     it('owner of first token should be address[0]', async () => {
@@ -32,7 +36,10 @@ contract('NftMarket', (accounts) => {
 
     it('tokenURI already used', async () => {
       await truffleAssert.fails(
-        _contract.mintToken(tokenURI, _nftPrice, { from: accounts[0] }),
+        _contract.mintToken(tokenURI, _nftPrice, {
+          from: accounts[0],
+          value: _listingPrice,
+        }),
         truffleAssert.ErrorType.REVERT,
         'Token URI already exists'
       );
@@ -41,6 +48,14 @@ contract('NftMarket', (accounts) => {
     it('should have one listed item', async () => {
       const listedItemsCount = await _contract.listedItemsCount();
       assert.equal(listedItemsCount.toNumber(), 1, 'listedItemsCount is not 1');
+    });
+
+    it('should have create NftItem', async () => {
+      const NftItem = await _contract.getNftItem(1);
+      assert.equal(NftItem.tokenId, 1, 'tokenId is not 1');
+      assert.equal(NftItem.price, _nftPrice, 'price is not correct');
+      assert.equal(NftItem.creator, accounts[0], 'creator ist not accounts[0]');
+      assert.equal(NftItem.isListed, true, 'isListed is not true');
     });
   });
 });
