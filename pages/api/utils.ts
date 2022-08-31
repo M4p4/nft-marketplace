bdc6442412c967e33a7fedc08d0776bd4ac344f7
M4p4 <homejr@protonmail.com>
2022-08-31 16:22:23 +0200

    wip

--- pages/api/utils.ts
+++ pages/api/utils.ts
@@ -1,11 +1,15 @@
+import { ethers } from 'ethers';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Session, withIronSession } from 'next-iron-session';
 import contract from '../../public/contracts/NftMarket.json';
+import { NftMarketContract } from '@_types/nftMarketContract';
+import * as util from 'ethereumjs-util';
 
 const NETWORKS = {
   '5777': 'GANACHE',
 };
 
+const abi = contract.abi;
 const targetNetwork = process.env
   .NEXT_PUBLIC_NETWORK_ID as keyof typeof NETWORKS;
 export const contractAddress = contract['networks'][targetNetwork]['address'];
@@ -24,9 +28,33 @@ export const addressCheckMiddleware = async (
   req: NextApiRequest & { session: Session },
   res: NextApiResponse
 ) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     const message = req.session.get('message-session');
-    if (message) {
+    const provider = new ethers.providers.JsonRpcProvider(
+      'http://127.0.0.1:7545'
+    );
+    const contract = new ethers.Contract(
+      contractAddress,
+      abi,
+      provider
+    ) as unknown as NftMarketContract;
+
+    if (!message) {
+      reject('Cannot read token');
+    }
+
+    let nonce: string | Buffer =
+      '\x19Ethereum Signed Message:\n' +
+      JSON.stringify(message).length +
+      JSON.stringify(message);
+
+    nonce = util.keccak(Buffer.from(nonce, 'utf-8'));
+    const { v, r, s } = util.fromRpcSig(req.body.signature);
+    const pubKey = util.ecrecover(util.toBuffer(nonce), v, r, s);
+    const addrBuffer = util.pubToAddress(pubKey);
+    const address = util.bufferToHex(addrBuffer);
+
+    if (address === req.body.address) {
       resolve('Correct Address');
     }
     reject('Wrong Address');
